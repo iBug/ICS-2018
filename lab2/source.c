70e646c819d171fafb15fe13cf73982bb959cb14
iBug <7273074+iBug@users.noreply.github.com>
2018-12-20 19:15:21 +0800

    add stuff

--- lab2/source.c
+++ lab2/source.c
@@ -5,7 +5,8 @@ i16 func(i16 n, i16 a, i16 b, i16 c, i16 d, i16 e, i16 f){  //Lots of arguments,
     i16 t = GETC() - '0' + a + b + c + d + e + f;
     if(n > 1){
         i16 x = func(n - 1, a, b, c, d, e, f);
-        i16 y = func(n - 2, a ,b, c, d, e, f); return x + y + t - 1;
+        i16 y = func(n - 2, a ,b, c, d, e, f);
+        return x + y + t - 1;
     }else{
         return t;
     }
